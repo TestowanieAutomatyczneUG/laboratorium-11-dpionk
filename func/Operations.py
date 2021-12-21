01246030866cf301c9294589fe619aed31a1ca56
Daria Pionk <dariapionk13@gmail.com>
2021-12-21 18:17:27 +0100

    Zadanie 1

---
+++ func/Operations.py
@@ -0,0 +1,23 @@
+import os 
+
+class Operations:
+
+	def readLines(self, filepath):
+		with open (filepath, 'r') as file:
+			data = []
+			for line in file:
+				data.append(line.strip())
+			return data
+
+	def writeSomethingInFile(self, filepath, toWrite):
+		with open (filepath, 'w') as file:
+			file.write(toWrite)
+		return toWrite
+	
+	def deleteFile(self, filepath):
+		if os.path.exists(filepath):
+			with open (filepath, 'r') as file:
+					os.remove(file)
+					return True
+		else:
+			raise ValueError('Wrong path')
